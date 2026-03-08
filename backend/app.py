653311844ce95de0b565dbea12d35314b9fa4ea2
L-nsamba <l.nsamba@alustudent.com>
2026-03-08 13:34:58 +0200

    adding admin call to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from backend.routes.auth_api import auth
+from backend.routes.admin import admin
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
     app.register_blueprint(auth, url_prefix="/api")
+    app.register_blueprint(admin, url_prefix="/api")
 
     return app
 

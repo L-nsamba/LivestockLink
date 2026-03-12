5d0bce68b46f2848d3e21c31d49de4828cfd701a
L-nsamba <l.nsamba@alustudent.com>
2026-03-12 16:41:36 +0200

    addition of the table creation calls from the newly implemented models tables

--- backend/database/database_setup.py
+++ backend/database/database_setup.py
@@ -2,6 +2,9 @@ from database.db import engine, Base
 from models.user import User
 from models.farmer import Farmer
 from models.transporter import Transporter
+from models.transport_request import TransportRequest
+from models.booking import Bookings
+from models.rating import Rating
 
 Base.metadata.create_all(engine)
 print("Tables created successfully")

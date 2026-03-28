e5f0aa2cd403b920a41cf9848b47565f9ce25020
L-nsamba <l.nsamba@alustudent.com>
2026-03-29 02:53:59 +0300

    debugging auth file registration issue

--- backend/routes/auth_api.py
+++ backend/routes/auth_api.py
@@ -77,7 +77,7 @@ def register():
 
         # Role-specific ID responses
         if data['role'] == 'FARMER':
-            user_data["farmer_id"] = farmer.farmer_id
+            user_data["farmer_id"] = farmer.user_id
         elif data['role'] == 'TRANSPORTER':
             user_data["transporter_id"] = transporter.transporter_id
 
@@ -141,7 +141,7 @@ def login():
 
         if user.role == 'FARMER':
             farmer = session.query(Farmer).filter_by(user_id=user.user_id).first()
-            user_data["farmer_id"] = farmer.farmer_id if farmer else None
+            user_data["farmer_id"] = farmer.user_id if farmer else None
         elif user.role  == 'TRANSPORTER':
             transporter = session.query(Transporter).filter_by(user_id=user.user_id).first()
             user_data["transporter_id"] = transporter.transporter_id if transporter else None
